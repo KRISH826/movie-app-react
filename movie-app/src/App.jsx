9b465a8b45f6662159edf7f456cc448613957da0
KRISH826 <krishnendupanj@gmail.com>
2024-03-27 10:24:53 +0530

    api_integration_done

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,10 +1,26 @@
 /** @format */
+import { useEffect } from "react";
 import "./App.scss";
+import { DatafromAPi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfig } from "./store/slices/HomeSlice";
 
 function App() {
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const apiTesting = () => {
+      DatafromAPi("movie/popular").then((res) => {
+        dispatch(getApiConfig(res));
+      });
+    };
+
+    apiTesting();
+  }, []);
+
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <h1>HELLO WORLD {url.total_pages}</h1>
     </>
   );
 }
--- movie-app/src/store/store.js
+++ movie-app/src/store/store.js
@@ -1,1 +1,9 @@
 /** @format */
+import { configureStore } from "@reduxjs/toolkit";
+import homeReducer from "./slices/HomeSlice";
+
+export const store = configureStore({
+  reducer: {
+    home: homeReducer,
+  },
+});
