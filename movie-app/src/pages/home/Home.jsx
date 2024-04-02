1c7b05ce12cb565c61625969f5ecdb24e499bb7d
KRISH826 <krishnendupanj@gmail.com>
2024-04-02 18:55:59 +0530

    push done

--- movie-app/src/pages/home/Home.jsx
+++ movie-app/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import HeroBanner from "../../components/HeroBanner";
 import Trending from "../../components/Trending";
 import Popular from "../../components/Popular";
+import TopRated from "../../components/TopRated";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <HeroBanner />
       <Trending />
       <Popular />
+      <TopRated />
     </section>
   );
 };
